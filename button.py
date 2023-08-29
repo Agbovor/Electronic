f62d1b7f8b977f84c9d29849cee59c5b06882ada
agbovor <agbovoralex65@gmail.com>
2023-08-29 17:13:25 +0100

    Button

--- button.py
+++ button.py
@@ -1,6 +1,22 @@
+# from gpiozero import Button
+
+# button = Button(2)
+
+# button.wait_for_press()
+# print("Button was pressed")
+
 from gpiozero import Button
+from signal import pause
+from send_message import IFTTT_NOTIFICATION
+
+newNotification = IFTTT_NOTIFICATION(eventName="send email", api_key= "kuonrPeTJC06djmqQ410mXu_ZZ7uMwBpx4Eg2F3chd3")
 
 button = Button(2)
 
-button.wait_for_press()
-print("Button was pressed")
+def button_pressed():
+    print("Button waas pressed")
+    newNotification.make_request()
+
+button.when_pressed = button_pressed
+
+pause()
--- send_message.py
+++ send_message.py
@@ -1,17 +1,31 @@
 import requests
+class IFTTT_NOTIFICATION:
+    def __init__(self, eventName, api_key, jsonData = None):
+        self.eventName = eventName
+        self.api_key = api_key
+        self.jsonData = jsonData
+
+    def make_request(self):
+        url = f"https://maker.ifttt.com/trigger/{self.eventName}/json/with/key/{self.api_key}"
+
+        try:
+            print("sending...")
+            response = requests.post(url, self.jsonData)
+            print(response)
+            print(response.text)
+            print("sent sucessfully")
+        except Exception as e:
+            print("run into an error", e)
+# def send_message():
 
 
-eventName = "send email"
-api_key = "kuonrPeTJC06djmqQ410mXu_ZZ7uMwBpx4Eg2F3chd3"
+#     eventName = "send email"
+#     api_key = "kuonrPeTJC06djmqQ410mXu_ZZ7uMwBpx4Eg2F3chd3"
 
-#create url
-url = (f"https://maker.ifttt.com/trigger/{eventName}/json/with/key/kuonrPeTJC06djmqQ410mXu_ZZ7uMwBpx4Eg2F3chd3")
+#     #create url
+#     url = (f"https://maker.ifttt.com/trigger/{eventName}/json/with/key/kuonrPeTJC06djmqQ410mXu_ZZ7uMwBpx4Eg2F3chd3")
 
-jsonData = {
-"Name": "Alex",
-"Age": "22"
-}
-
-response = requests.post(url, jsonData)
-
-print(response.text)
+#     jsonData = {
+#     "Name": "Alex",
+#     "Age": "22"
+#     }
